c224cea47b0a83184a9b1c9c539e4c42738f42e8
kunkaffa <kunkaffa3@gmail.com>
2025-08-30 18:04:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -83,7 +83,7 @@ def initHeaders():
 
 def handleStatusCodes(status_code):
 	global request_counter
-	sys.stdout.write:
+	sys.stdout.write
 	print("\033[33mB A S E - L7 \033[37m" +(str)+ " \033[31mREQUEST SENT \033[0m(\r%i % "request_counter)
 	sys.stdout.flush()
 	if status_code == 429:
