3efe749dc3b55df8a2a8b94e473aac863686501b
kunkaffa <kunkaffa3@gmail.com>
2025-08-30 22:02:30 +0700

    Update run.py

--- run.py
+++ run.py
@@ -83,7 +83,8 @@ def initHeaders():
 
 def handleStatusCodes(status_code):
 	global request_counter
-	sys.stdout.write("\r%i ATTACK REQUEST SENT" % request_counter)
+	sys.stdout.write
+	     print("\r%i \033[92mATTACK REQUEST SENT\033[0m" % request_counter)
 	sys.stdout.flush()
 	if status_code == 429:
 			printMsg("You have been throttled")
